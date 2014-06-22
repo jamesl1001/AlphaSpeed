ebb7f73ae5321edd77a6a4ef03302a87bdc2fa4f
jamesl1001 <jamesl1001@msn.com>
2014-06-22 17:31:55 +0100

    Check referer to help prevent external score submissions

--- php/saveScore.php
+++ php/saveScore.php
@@ -1,16 +1,20 @@
 <?php
 
-require_once('db.php');
+require_once('hashes.php');
 
-$name  = $_POST['name'];
-$score = $_POST['score'];
-$game  = $_POST['game'];
+if(in_array(md5($_SERVER['HTTP_REFERER']), $hashes, true)) {
+	require_once('db.php');
 
-$timestamp = date("Y-m-d H:i:s");
+	$name  = $_POST['name'];
+	$score = $_POST['score'];
+	$game  = $_POST['game'];
 
-$sth = $dbh->prepare("INSERT INTO scores (name, score, game, timestamp) value (:name, :score, :game, :timestamp)");
-$sth->bindParam(':name', $name);
-$sth->bindParam(':score', $score);
-$sth->bindParam(':game', $game);
-$sth->bindParam(':timestamp', $timestamp);
-$sth->execute();
+	$timestamp = date("Y-m-d H:i:s");
+
+	$sth = $dbh->prepare("INSERT INTO scores (name, score, game, timestamp) value (:name, :score, :game, :timestamp)");
+	$sth->bindParam(':name', $name);
+	$sth->bindParam(':score', $score);
+	$sth->bindParam(':game', $game);
+	$sth->bindParam(':timestamp', $timestamp);
+	$sth->execute();
+}
